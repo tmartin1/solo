05ff52ef4b608628f98b2cabfb22850b5ebed33e
Tim Martin <timothy.m.martin@gmail.com>
2015-01-19 13:12:55 -0800

    Added template 'home' route, controller, and other files.

---
+++ client/app/home/home.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('teamforgeApp')
+  .controller('HomeCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/home/home.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('teamforgeApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/home', {
+        templateUrl: 'app/home/home.html',
+        controller: 'HomeCtrl'
+      });
+  });
