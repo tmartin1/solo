19ecac1776cdcdf5df342f02aee7f90b6aa0a640
Tim Martin <timothy.m.martin@gmail.com>
2015-01-20 10:28:35 -0800

    Requires projects to have unique names on creation.

--- client/app/project/create-project/create-project.controller.js
+++ client/app/project/create-project/create-project.controller.js
@@ -3,13 +3,24 @@
 angular.module('teamforgeApp')
   .controller('CreateProjectCtrl', function ($scope, $http, $location, Auth) {
     
+    // Check to make sure a project with the proposed title doesn't already exist.
+    $scope.checkTitle = function() {
+      $http.get( '/api/projects', {params: {title: $scope.newTitle}} )
+        .success(function(projects) {
+          alert('A project with that name already exists, please choose a different title.');
+        })
+        .error(function(error) {
+          $scope.createProject();
+        });
+    };
+
     $scope.createProject = function() {
       $http.post('/api/projects', {
-      	title: $scope.newTitle,
-      	summary: $scope.newSummary,
-      	details: $scope.newDetails,
-      	ownerName: Auth.getCurrentUser().name,
-      	_ownerId: Auth.getCurrentUser()._id
+        title: $scope.newTitle,
+        summary: $scope.newSummary,
+        details: $scope.newDetails,
+        ownerName: Auth.getCurrentUser().name,
+        _ownerId: Auth.getCurrentUser()._id
       });
       // Redirect user to home page when project is created.
       $location.path('/home');
