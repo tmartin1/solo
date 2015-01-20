de8107f56bcf4a434cf2d0382249b19024477f24
Tim Martin <timothy.m.martin@gmail.com>
2015-01-19 17:04:09 -0800

    Created projects now properly store owner id and name.

--- client/app/project/create-project/create-project.controller.js
+++ client/app/project/create-project/create-project.controller.js
@@ -1,15 +1,17 @@
 'use strict';
 
 angular.module('teamforgeApp')
-  .controller('CreateProjectCtrl', function ($scope, $http) {
+  .controller('CreateProjectCtrl', function ($scope, $http, Auth, $location) {
     
     $scope.createProject = function() {
       $http.post('/api/projects', {
       	title: $scope.newTitle,
       	summary: $scope.newSummary,
       	details: $scope.newDetails,
-      	owenerName: 'Owner',
-      	_ownerId: 'id'
+      	ownerName: Auth.getCurrentUser().name,
+      	_ownerId: Auth.getCurrentUser()._id
       });
+      // Redirect user to home page when project is created.
+      $location.path('/home');
     };
   });
