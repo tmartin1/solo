81539393f3f93ace2d9986c3c9371618b21ea0af
Tim Martin <timothy.m.martin@gmail.com>
2015-01-20 15:37:46 -0800

    Fixed broken links on main page.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,12 +3,9 @@
 angular.module('teamforgeApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.gettingStarted = [
-      { name: 'Join a Group', link: '' },
-      { name: 'Start a Group', link: '' },
-      { name: 'View Projects in Your Group', link: 'home' },
-      { name: 'View Public Projects', link: 'home' },
+      { name: 'View Projects', link: 'home' },
       { name: 'Create a Project', link: 'create-project' },
-      { name: 'Review Your Projects', link: '' }
+      { name: 'Review Your Projects', link: 'my-projects' }
     ];
 
   });
