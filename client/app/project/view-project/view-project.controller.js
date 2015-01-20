945d00630b0666de83717f629f788a0d0f0eb25c
Tim Martin <timothy.m.martin@gmail.com>
2015-01-20 10:39:23 -0800

    Added 'edit project' button that is only visible if project owner is viewing the project.

--- client/app/project/view-project/view-project.controller.js
+++ client/app/project/view-project/view-project.controller.js
@@ -3,21 +3,26 @@
 angular.module('teamforgeApp')
   .controller('ViewProjectCtrl', function ($scope, $http, $location, Auth) {
     
+    var currentUser = Auth.getCurrentUser().name;
+    $scope.isOwner = false;
+
     var getProject = function() {
 	    $http.get('/api/projects/'+$location.hash())
 	    	.success(function(project) {
 	      	$scope.project = project;
+          $scope.isOwner = project.ownerName === currentUser;
 	    	})
 	    	.error(function(error) {
 			    console.log('ERROR: ', error);
 			  });
-	  };
-	  getProject();
+    };
+    getProject();
+
 
     // Allows users to add comments to a project
     $scope.addComment = function() {
     	var comment = {
-    		username: Auth.getCurrentUser().name,
+    		username: currentUser,
     		text: $scope.newComment
     	};
     	getProject();
